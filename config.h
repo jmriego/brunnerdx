d303680411db227ff6689bd6e39fa16d65ee3b60
Jose Valenzuela <jvalenzuela@indeed.com>
2020-11-04 18:25:52 +0000

    fix config.h

--- config.h
+++ config.h
@@ -8,21 +8,3 @@ const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 #define brunnerIP IPAddress (192, 168, 3, 194)
 #define ip IPAddress (192, 168, 3, 167)
 #define port 15090
-
-// --------------------------
-// Joystick related variables
-// --------------------------
-
-#define default_gain 100
-#define totalGain default_gain
-#define constantGain default_gain
-#define rampGain default_gain
-#define squareGain default_gain
-#define sineGain default_gain
-#define triangleGain default_gain
-#define sawtoothdownGain default_gain
-#define sawtoothupGain default_gain
-#define springGain default_gain
-#define damperGain default_gain
-#define inertiaGain default_gain
-#define frictionGain default_gain
--- libraries/Joystick/Joystick.cpp
+++ libraries/Joystick/Joystick.cpp
@@ -19,10 +19,28 @@
 */
 
 #include "Joystick.h"
-#include "../config.h"
 #include "FFBDescriptor.h"
 #include "filters.h"
 #if defined(_USING_DYNAMIC_HID)
+
+
+// --------------------------
+// Joystick related variables
+// --------------------------
+
+#define default_gain 100
+#define totalGain default_gain
+#define constantGain default_gain
+#define rampGain default_gain
+#define squareGain default_gain
+#define sineGain default_gain
+#define triangleGain default_gain
+#define sawtoothdownGain default_gain
+#define sawtoothupGain default_gain
+#define springGain default_gain
+#define damperGain default_gain
+#define inertiaGain default_gain
+#define frictionGain default_gain
 
 #define JOYSTICK_REPORT_ID_INDEX 7
 #define JOYSTICK_AXIS_MINIMUM -32767
